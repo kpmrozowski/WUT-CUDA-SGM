951fa271155445ef7a1191cf370516be0aba4f72
kpmrozowski <kpmrozowski@gmail.com>
2021-11-14 23:43:52 +0100

    added cesus transform

--- src/census_transform.cu
+++ src/census_transform.cu
@@ -5,6 +5,9 @@ namespace sgm {
 
 namespace {
 
+static constexpr int CESUS_WINDOW_WIDTH  = 5;
+static constexpr int CESUS_WINDOW_HEIGHT = 5;
+
 template <typename T>
 __global__ void census_transform_kernel(
 	feature_type *dest,
@@ -12,7 +15,43 @@ __global__ void census_transform_kernel(
 	int width,
 	int height,
 	int pitch)
-{}
+{
+	using pixel_type = T;
+	const int padX = CESUS_WINDOW_WIDTH / 2;
+	const int padY = CESUS_WINDOW_HEIGHT / 2;
+	const int x = threadIdx.x;
+	const int y = threadIdx.y + blockIdx.y * blockDim.y;
+	feature_type f = 1;
+	if (padX <= x && x < width - padX && padY <= y && y < height - padY) {
+		const T center = src[x + y * width];
+        int bitPos0 = 0;
+		int bitPos1 = CESUS_WINDOW_WIDTH * CESUS_WINDOW_HEIGHT - 1;
+		for (int win_y = 0; win_y < CESUS_WINDOW_HEIGHT; ++win_y, ++bitPos0, --bitPos1) {
+			for (int win_x = 0; win_x < CESUS_WINDOW_WIDTH; ++win_x) {
+				const T srcVal0 = src[ x + win_x + (y + win_y) * width ];
+				const T srcVal1 = src[ x - win_x + (y - win_y) * width ];
+				f |= (center < srcVal0) << bitPos0;
+				f |= (center < srcVal1) << bitPos1;
+			}
+		}
+		dest[x + y * width] = f;
+	}
+	// else if ( (0 <= x && x < padX || width - padX <= x && x < width) 
+	// 	   || (0 <= y && y < padY || height -padY <= y && y < height) ) {
+	// 		dest[x + y * width] = 0;
+	// }
+}
+int nextPowerOf2(int n)
+{
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    n++;
+    return n;
+}
 
 template <typename T>
 void enqueue_census_transform(
@@ -24,6 +63,24 @@ void enqueue_census_transform(
 	cudaStream_t stream)
 {
 	printf("My cesus transform\n");
+    int num_threads = 1024;
+	int block_x_dim = nextPowerOf2(width);
+	int block_y_dim = num_threads / block_x_dim;
+	int grid_dim = height / block_y_dim;
+	if (grid_dim * block_y_dim < height) {
+		grid_dim += 1;
+	}
+	const dim3 bdim(block_x_dim, block_y_dim);
+	const dim3 gdim(1, grid_dim, 1);
+
+	
+	census_transform_kernel<<<gdim, bdim, 0, stream>>>(dest, src, width, height, pitch);
+#ifdef DEBUG
+	feature_type lookup[height * width];
+	printf("dest.size()=%d\n", sizeof(lookup)/sizeof(lookup[0]));
+	cudaMemcpy(lookup, dest, sizeof(feature_type) * height * width, cudaMemcpyDeviceToHost);
+	printf("dest[100][100]=%d\n", lookup[300 + 300 * width]);
+#endif
 }
 
 }
